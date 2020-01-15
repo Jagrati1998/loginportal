218b752e064fe245e71d9e870a792d41fa58f711
Jagrati1998 <48794893+Jagrati1998@users.noreply.github.com>
2020-01-15 22:47:51 +0530

    Add files via upload

---
+++ UserController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RegistrationForm.Models;
+
+
+namespace RegistrationForm.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        // Registration action
+        [HttpGet]
+
+        public ActionResult Registration()
+        {
+            return View();
+        }//
+         /*  
+           [HttpPost]
+           [ValidateAntiForgeryToken]
+         public ActionResult Registration(RegistrationForm.Models.User user)
+           {
+               bool Status = false;
+               String Message = " ";
+               if (ModelState.IsValid)
+               {
+                   // Email is already Exist or Not
+
+               }
+
+               else
+               {
+                   Message = "InvalidRequest";
+               }
+               return View(user);
+           }
+           [NonAction]
+           public bool IsEmailExicst(string emailID)
+           {
+               using (DatabaseEntities dc = new DatabaseEntities())
+                   var v = dc.Users.Where(a => a.EmailID == EmailID()).FirstOrDefault);
+
+           }*/
+    }
+    
+}
